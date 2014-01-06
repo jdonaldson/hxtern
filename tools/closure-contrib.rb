b5a363a464fa3673ffaa603166321cf018604eae
Justin Donaldson <jdonaldson@gmail.com>
2014-01-06 07:55:34 -0800

    more wip

--- tools/closure-contrib.rb
+++ tools/closure-contrib.rb
@@ -1,56 +1,108 @@
 input_dir, output_dir = ARGV
 root_dir = Dir.pwd
 
-if input_dir.nil?  then input_dir  = '../ext/closure-compiler/contrib/externs' end
-if output_dir.nil? then output_dir = '../gen_src' end
+if input_dir.nil?  then input_dir  = 'ext/closure-compiler/contrib/externs' end
+if output_dir.nil? then output_dir = 'gen_src' end
+
 
 Dir.chdir(input_dir)
 
-Dir.glob("*.js").each { |f| 
-    contents = File.open(f).read
-    Dir.chdir(root_dir)
-    Dir.chdir(output_dir)
-    target_dir = f.split('.')[0]
-    Dir.mkdir(target_dir)
-    declarations = contents.split(/;\n\s+$/)
-    namespace = Hash.new
-    declarations.each{ |d|
-        Dir.chdir(root_dir)
-        Dir.chdir(output_dir)
-        Dir.chdir(target_dir)
-        # split on the first word character, which should be the start of an expression (everything else is comments in these files)
-        doc, sig = d.split(/(?=^\w)/) 
-
-        # split on "="  for the signature which should give assignments to variable names
-        name, assignment = sig.split(/\s*=\s*/)
-
-        # split on prototype, which will give us instance fields for a class
-        package, instance_field = name.split('.prototype.')
-
-        if not instance_field.nil? then
-           packages = package.split('.') # split on packages 
-           marker = namespace
-           packages.each {|p|
-               if marker[p].nil? then marker[p] = Hash.new end
-               marker = marker[p]
-           }
-        end
-        classes = []
-        if not packages.nil? then
-            while packages[-1]=~ /^[A-Z]/ 
-                classes.unshift(packages.pop)
+Dir.glob("*.js").each do |f| 
+    stack = []
+    File.open(f) do |fh|
+        while (line = fh.gets)
+            if line =~ /^\/\*\*/ then
+                stack.push({:doc_start => line})
+            elsif line =~ /^\s*\*\// then
+                stack.push({:doc_end => line})
+            elsif  line =~ /^\s*$/ then
+                nil
+            else
+                stack.push({:other => line})
             end
-            packages.each {|x|
-                if not Dir.exists?(x) then Dir.mkdir(x) end
-                Dir.chdir(x)
-            }
         end
-        if not classes.empty? then 
-            File.open(classes.shift + '.hx', 'w') { |file| file.write('some text!')}
+
+    end
+    chunks = []
+    current = []
+    
+    # sort out the doc start/end and other lines into doc/sig groups
+    chunk = {:doc => [], :sig => []}
+    stack.each do |s|
+        if s.has_key?(:doc_start) then
+            chunks.push(chunk)
+            chunk = {:doc => [], :sig => []}
+            chunk[:doc].push(s[:doc_start])
+            current = chunk[:doc]
+        elsif s.has_key?(:doc_end) then
+            chunk[:doc].push(s[:doc_end])
+            current = chunk[:sig]
+        elsif s.has_key?(:other) then
+            current.push(s[:other])
         end
-          
+    end
+
+    # collapse the array of doc and signatures into single strings
+    declarations = chunks.map {|c|
+        {:doc => c[:doc].join, :sig => c[:sig].join.sub("\n", "") }
     }
-    namespace.each {|x,y|
+
+    # sort out packages, classes, and expressions 
+    package_directory = {}
+    declarations.select! {|c| not (c.nil? or c[:sig].nil? or c[:sig] == '')  }
+    declarations.map! {|c|
+        # p ''
+        # p 'doc:' + c[:doc]
+        # p 'sig:' + c[:sig]
+        c[:field], c[:expression] = c[:sig].split(/\s*=\s*/)
+        c[:field].sub!(/;/,'')
+        c
     }
+
+    declarations.map! { |c|
+        c[:classes] = []
+
+        # handle constructors
+        # constructor_match = (c[:field] =~  /^\s*function\s*([\w\.]+)/)
+        constructor_match = /^\s*function\s*([\w\.]+)/.match(c[:field])
+        if constructor_match then 
+           c[:field] = constructor_match[1] + '.prototype.new'
+        end
+
+        # handle global variable declarations
+        var_match = /^\s*var\s*([\w\.]+)/.match(c[:field])
+        if var_match then
+            c[:field] = var_match[1]
+            c[:classes].push("global")
+            c[:native] = c[:field]
+        end
+
+        c[:package_class], c[:instance_method] = c[:field].split(".prototype.")
+        c[:packages] = c[:package_class].split('.')
+
+        # handle "package" level methods... use a dummy class and @:native
+        if c[:packages][-1] =~ /^[a-z]/ and not c[:expression].nil?
+            original = c[:packages][-1]
+            dummy_class = original.split(/(\W)/).map(&:capitalize).join
+            c[:classes].push(dummy_class)
+            c[:native] = original
+        end
+
+        while c[:packages][-1] =~ /^[A-Z]/
+            c[:classes].unshift(c[:packages].pop)
+        end
+
+        current_package = package_directory
+        c[:packages].each do |p|
+            if current_package[p].nil? then current_package[p] = {} end
+            current_package = current_package[p]
+        end
+
+        c
+    }
+p package_directory
     break
-}
+end
+
+
+# chunks.each {|x| p x['sig'].join}
