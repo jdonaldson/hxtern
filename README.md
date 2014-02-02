ab41d5b5deced9e3a2cb471ba2a0b14f120b4967
Justin Donaldson <jdonaldson@gmail.com>
2014-02-02 12:26:54 -0800

    add initial externs

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ hxtern
 ======
 
 A repository of automatically generated Haxe externs for js
+
+Please see [jsdoc3-hxtern](https://github.com/jdonaldson/jsdoc3-hxtern) for the
+plugin that generates these externx.
---
+++ tools/genall.rb
@@ -0,0 +1,13 @@
+cur_folder = Dir.getwd
+base_folder = "ext/closure-compiler/contrib/externs"
+template = "~/Projects/jsdoc3-hxtern/template"
+output_folder = File.expand_path File.join(cur_folder, "gen_src")
+
+Dir.chdir(base_folder)
+
+files = File.join( "**", "*.js")
+Dir.glob(files) do |file|
+    output_dir = File.dirname(file)
+    output = File.join(output_folder, output_dir , File.basename(file, '.*'))
+    `jsdoc -t #{template} #{file} -d #{output} 2>`
+end
