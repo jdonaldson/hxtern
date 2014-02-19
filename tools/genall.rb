49754c9788eed2fb75ba4f82c1d8fca4a4d16853
Justin Donaldson <jdonaldson@gmail.com>
2014-02-18 23:15:03 -0800

    update tools with new global argument

--- tools/genall.rb
+++ tools/genall.rb
@@ -8,7 +8,11 @@ Dir.chdir(base_folder)
 files = File.join( "**", "*.js")
 Dir.glob(files) do |file|
     output_dir = File.dirname(file)
-    output = File.join(output_folder, output_dir , File.basename(file, '.*'))
-    p file
-    `jsdoc -t #{template} #{file} -d #{output}`
+    basename = File.basename(file, '.*')
+    base, version = basename.split("-")
+    potential_packs = base.split("_")
+    packs = potential_packs.reject{ |x| x.match(/^v\d+/) or x.match(/^\d+/) }
+    global_pack =  packs.join('.')
+    output = File.join(output_folder, output_dir , basename)
+    `jsdoc -t #{template} #{file} -d #{output} -q global=#{global_pack}`
 end
