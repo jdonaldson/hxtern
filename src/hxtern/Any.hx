a1a704a363411abf8872b1987b95a5a6a2601efb
Justin Donaldson <jdonaldson@gmail.com>
2014-02-22 16:44:07 -0800

    fix Any type

--- src/hxtern/Any.hx
+++ src/hxtern/Any.hx
@@ -3,4 +3,4 @@ abstract Any2<T1, T2>(Dynamic) from T1 from T2 to T1 to T2 {}
 abstract Any3<T1, T2, T3>(Dynamic) from T1 from T2 from T3 to T1 to T2 to T3 {}
 abstract Any4<T1, T2, T3, T4>(Dynamic) from T1 from T2 from T3 from T4 to T1 to T2 to T3 to T4 {}
 abstract Any5<T1, T2, T3, T4, T5>(Dynamic) from T1 from T2 from T3 from T4 from T5 to T1 to T2 to T3 to T4 to T5 {}
-abstract Any5<T1, T2, T3, T4, T5, T6>(Dynamic) from T1 from T2 from T3 from T4 from T5 from T6 to T1 to T2 to T3 to T4 to T5 to T6 {}
+abstract Any6<T1, T2, T3, T4, T5, T6>(Dynamic) from T1 from T2 from T3 from T4 from T5 from T6 to T1 to T2 to T3 to T4 to T5 to T6 {}
