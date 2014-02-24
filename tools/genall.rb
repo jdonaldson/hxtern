2d401e305543c2988bbc35e53b7652a47cfa9327
Justin Donaldson <jdonaldson@gmail.com>
2014-02-23 17:56:05 -0800

    add libmeta/ignore information, use it on jquery, et al

--- tools/genall.rb
+++ tools/genall.rb
@@ -1,8 +1,9 @@
+require "json"
 cur_folder = Dir.getwd
 base_folder = "ext/closure-compiler/contrib/externs"
 template = "~/Projects/jsdoc3-hxtern/template"
 output_folder = File.expand_path File.join(cur_folder, "gen_src")
-
+libmeta = JSON.parse(IO.read("libmeta.json"));
 Dir.chdir(base_folder)
 
 files = File.join( "**", "*.js")
@@ -14,5 +15,18 @@ Dir.glob(files) do |file|
     packs = potential_packs.reject{ |x| x.match(/^v\d+/) or x.match(/^\d+/) }
     global_pack =  packs.join('.')
     output = File.join(output_folder, output_dir , basename)
-    `jsdoc -t #{template} #{file} -d #{output} -q global=#{global_pack}`
+    filename = File.basename(file)
+    ignore = libmeta['ignore']
+    ignore_list = ''
+    if ignore
+        ignore_list = ignore[File.basename(file)]
+        if ignore_list == nil
+            ignore_list = ''
+        else
+            p ignore_list
+        end
+    end
+    command = "jsdoc -t #{template} #{file} -d #{output} -q 'global=#{global_pack}&ignore=#{ignore_list}'"
+    p command
+    `#{command}`
 end
