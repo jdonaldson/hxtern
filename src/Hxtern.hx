ba9304c0983e961eb6215adb873c3007feab8e20
Justin Donaldson <jdonaldson@gmail.com>
2014-02-23 20:12:54 -0800

    add a few more variants to Hxtern.Any

--- src/Hxtern.hx
+++ src/Hxtern.hx
@@ -3,3 +3,5 @@ abstract Any3<T1, T2, T3>(Dynamic) from T1 from T2 from T3 to T1 to T2 to T3 {}
 abstract Any4<T1, T2, T3, T4>(Dynamic) from T1 from T2 from T3 from T4 to T1 to T2 to T3 to T4 {}
 abstract Any5<T1, T2, T3, T4, T5>(Dynamic) from T1 from T2 from T3 from T4 from T5 to T1 to T2 to T3 to T4 to T5 {}
 abstract Any6<T1, T2, T3, T4, T5, T6>(Dynamic) from T1 from T2 from T3 from T4 from T5 from T6 to T1 to T2 to T3 to T4 to T5 to T6 {}
+abstract Any7<T1, T2, T3, T4, T5, T6, T7>(Dynamic) from T1 from T2 from T3 from T4 from T5 from T6 to T1 to T2 to T3 to T4 to T5 to T6 to T7{}
+abstract Any7<T1, T2, T3, T4, T5, T6, T7, T8>(Dynamic) from T1 from T2 from T3 from T4 from T5 from T6 to T1 to T2 to T3 to T4 to T5 to T6 to T7 to T8{}
